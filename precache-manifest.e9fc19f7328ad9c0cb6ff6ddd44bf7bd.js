4f904e4a28c9891fc195e8ef0b448c725547428c
yg-yash <yg123yash@gmail.com>
2020-09-03 02:01:30 +0530

    Updates

---
+++ precache-manifest.e9fc19f7328ad9c0cb6ff6ddd44bf7bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31a0bd4d663491bab9118f070807e40f",
+    "url": "/99acres-clone/index.html"
+  },
+  {
+    "revision": "d2da0631d28e417384fb",
+    "url": "/99acres-clone/static/css/main.04a8d146.chunk.css"
+  },
+  {
+    "revision": "1719e8edfc8c407345d6",
+    "url": "/99acres-clone/static/js/2.909da37d.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/99acres-clone/static/js/2.909da37d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2da0631d28e417384fb",
+    "url": "/99acres-clone/static/js/main.336b82e0.chunk.js"
+  },
+  {
+    "revision": "fa9172f9587bf4c34541",
+    "url": "/99acres-clone/static/js/runtime-main.b68cefcd.js"
+  },
+  {
+    "revision": "fe86c735b0bb9c38da5a8457449e494b",
+    "url": "/99acres-clone/static/media/dashboardImage.fe86c735.jpg"
+  }
+]);
